cd17f73bcf3f469191e502a51fbf9d5498803a08
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-12-27 15:34:06 -0800

    Attach another asteroid to the existing one

--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -5,6 +5,8 @@ public class Engine : MonoBehaviour
 {
 	public float deltaV;
 	public float deltaW;
+	public float maxDeltaV;
+	public float maxDeltaW;
 	public Rigidbody2D rigidbody2d;
 	// Use this for initialization
 	void Start ()
@@ -12,7 +14,7 @@ public class Engine : MonoBehaviour
 	
 	}
 	// Update is called once per frame
-	void Update ()
+	void LateUpdate ()
 	{
 		if (Input.GetKey (KeyCode.W)) {
 			rigidbody2d.AddForce ((transform.rotation * Vector2.up) * deltaV * Time.deltaTime);
@@ -29,5 +31,8 @@ public class Engine : MonoBehaviour
 		if (Input.GetKey (KeyCode.D)) {
 			rigidbody2d.AddTorque (-deltaW * Time.deltaTime);
 		}
+
+		rigidbody2d.angularVelocity = Mathf.Min (rigidbody2d.angularVelocity, maxDeltaW);
+		rigidbody2d.velocity = Vector2.ClampMagnitude (rigidbody2d.velocity, maxDeltaV);
 	}
 }
